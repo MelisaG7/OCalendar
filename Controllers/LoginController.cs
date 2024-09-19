3fb5816ce7c834ff2b64716e90d5ffc0e7470825
wen <wen.bei@outlook.com>
2024-09-19 19:06:02 +0200

    1.1 implemented

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -20,19 +20,46 @@ public class LoginController : Controller
     public IActionResult Login([FromBody] LoginBody loginBody)
     {
         // TODO: Impelement login method
-        return Unauthorized("Incorrect password");
+        if (loginBody.Username is null)
+        {
+            return BadRequest("Username was left empty");
+        }
+        else if (loginBody.Password is null)
+        {
+            return BadRequest("Password was left empty");
+        }
+        var loginStatus = _loginService.CheckPassword(loginBody.Username, loginBody.Password);
+
+        // Handle login result
+        switch (loginStatus)
+        {
+            case LoginStatus.Success:
+                return Ok("Login successful");
+            case LoginStatus.IncorrectUsername:
+                return Unauthorized("Username not found");
+            case LoginStatus.IncorrectPassword:
+                return Unauthorized("Incorrect password");
+            default:
+                return BadRequest("Unknown error");
+    }
     }
 
     [HttpGet("IsAdminLoggedIn")]
     public IActionResult IsAdminLoggedIn()
     {
-        // TODO: This method should return a status 200 OK when logged in, else 403, unauthorized
-        return Unauthorized("You are not logged in");
+        // TODO: This method should return a status 200 OK when logged in, else 403, unauthorized 
+        if (_loginService.CheckAdminLoggedIn())
+        {
+            var adminUsername = _loginService.GetLoggedInAdminUsername();
+            return Ok(adminUsername);
+        }
+        return Unauthorized("Admin not logged in");
     }
 
     [HttpGet("Logout")]
     public IActionResult Logout()
     {
+        _loginService.Logout();
         return Ok("Logged out");
     }
 
