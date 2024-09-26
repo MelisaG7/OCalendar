08dc0c484a8bfa582428606dfa969bc15251ed65
MelisaG7 <melisa_gunde@hotmail.com>
2024-09-26 18:52:11 +0200

    1.3 Login and registration of new users.

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -5,21 +5,7 @@ namespace StarterKit.Models
 {
     public class DatabaseContext : DbContext
     {
-        // The admin table will be used in both cases
         public DbSet<Admin> Admin { get; set; }
-
-        // You can comment out or remove the case you are not going to use.
-
-        // Tables for the Theatre ticket case
-
-        // public DbSet<Customer> Customer { get; set; }
-        // public DbSet<Reservation> Reservation { get; set; }
-        // public DbSet<TheatreShowDate> TheatreShowDate { get; set; }
-        // public DbSet<TheatreShow> TheatreShow { get; set; }
-        // public DbSet<Venue> Venue { get; set; }
-
-        // Tables for the event calendar case
-
         public DbSet<User> User { get; set; }
         public DbSet<Attendance> Attendance { get; set; }
         public DbSet<Event_Attendance> Event_Attendance { get; set; }
@@ -50,8 +36,13 @@ namespace StarterKit.Models
 
             modelBuilder.Entity<Event>();
             modelBuilder.Entity<Event>().HasData(
-                new{ EventId = 101, Title = "Tech Conference", Description = "A conference about the latest in tech.", EventDate = new DateOnly(2024, 10, 25), StartTime = new TimeSpan(9, 0, 0), EndTime = new TimeSpan(17, 0, 0), Location = "Convention Center", AdminApproval = true, Event_Attendances = ""}
+                new { EventId = 101, Title = "Tech Conference", Description = "A conference about the latest in tech.", EventDate = new DateOnly(2024, 10, 25), StartTime = new TimeSpan(9, 0, 0), EndTime = new TimeSpan(17, 0, 0), Location = "Convention Center", AdminApproval = true, Event_Attendances = "" }
             );
+
+            modelBuilder.Entity<User>().HasData(
+             new User { UserId = 1, FirstName = "John", LastName = "Doe", Email = "john.doe@example.com", Password = EncryptionHelper.EncryptPassword("password"), RecuringDays = "mo,tu,we", Attendances = [], Event_Attendances = [] },
+            new User { UserId = 2, FirstName = "Jane", LastName = "Smith", Email = "jane.smith@example.com", Password = EncryptionHelper.EncryptPassword("mypassword"), RecuringDays = "th,fr", Attendances = [], Event_Attendances = [] }
+);
 
         }
 
--- Services/LoginService.cs
+++ Services/LoginService.cs
@@ -6,7 +6,7 @@ namespace StarterKit.Services;
 
 public enum LoginStatus { IncorrectPassword, IncorrectUsername, Success }
 
-public enum ADMIN_SESSION_KEY { adminLoggedIn }
+public enum SESSION_KEY { adminLoggedIn, userLoggedIn }
 
 public class LoginService : ILoginService
 {
@@ -23,37 +23,63 @@ public class LoginService : ILoginService
     {
         // TODO: Make this method check the password with what is in the database
         // check if username exists
-        
-        var admin =  _context.Admin.FirstOrDefault(p => p.UserName == username);
+        bool userlogin = false;
+        var admin = _context.Admin.FirstOrDefault(p => p.UserName == username);
+        var user = _context.User.FirstOrDefault(p => p.Email == username);
 
-        if (admin is null)
+        if (admin is null && user is null)
             return LoginStatus.IncorrectUsername;
-
+        else if (admin is null)
+        {
+            userlogin = true;
+        }
+        // check wether the user is trying to login or the admin
         // check password
         var encryptedPassword = EncryptionHelper.EncryptPassword(inputPassword);
 
-        if (admin.Password == encryptedPassword)
+        if (!userlogin && admin.Password == encryptedPassword)
         {
-            _httpContextAccessor.HttpContext.Session.SetString(ADMIN_SESSION_KEY.adminLoggedIn.ToString(), admin.UserName);
+            _httpContextAccessor.HttpContext.Session.SetString(SESSION_KEY.adminLoggedIn.ToString(), admin.UserName);
+            return LoginStatus.Success;
+        }
+        else if (user.Password == encryptedPassword)
+        {
+            _httpContextAccessor.HttpContext.Session.SetString(SESSION_KEY.userLoggedIn.ToString(), user.Email);
             return LoginStatus.Success;
         }
         return LoginStatus.IncorrectPassword;
     }
     public bool CheckAdminLoggedIn()
     {
-        var username = _httpContextAccessor.HttpContext.Session.GetString(ADMIN_SESSION_KEY.adminLoggedIn.ToString());
+        var username = _httpContextAccessor.HttpContext.Session.GetString(SESSION_KEY.adminLoggedIn.ToString());
+        return !string.IsNullOrEmpty(username);
+    }
+
+    public bool CheckUserLoggedIn()
+    {
+        var username = _httpContextAccessor.HttpContext.Session.GetString(SESSION_KEY.userLoggedIn.ToString());
         return !string.IsNullOrEmpty(username);
     }
 
     public string GetLoggedInAdminUsername()
     {
-        var username = _httpContextAccessor.HttpContext.Session.GetString(ADMIN_SESSION_KEY.adminLoggedIn.ToString());
+        var username = _httpContextAccessor.HttpContext.Session.GetString(SESSION_KEY.adminLoggedIn.ToString());
         if (username is not null)
             return username;
         return null;
     }
-     public void Logout()
+    public void Logout()
     {
-        _httpContextAccessor.HttpContext.Session.Remove(ADMIN_SESSION_KEY.adminLoggedIn.ToString());
+        _httpContextAccessor.HttpContext.Session.Remove(SESSION_KEY.adminLoggedIn.ToString());
+    }
+    public void LogoutUser()
+    {
+        _httpContextAccessor.HttpContext.Session.Remove(SESSION_KEY.userLoggedIn.ToString());
+    }
+
+    public async Task AddUserToDb(User user)
+    {
+        await _context.User.AddAsync(user);
+        await _context.SaveChangesAsync();
     }
 }
